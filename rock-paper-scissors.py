29158857af1106e4e7094ce0365bf01ef22a5a18
Therine <krpdelong@gmail.com>
2021-11-10 09:44:27 -0600

    This program runs one iteration of the game rock paper scissors.

---
+++ rock-paper-scissors.py
@@ -0,0 +1,30 @@
+from random import randrange 
+number = randrange(3) 
+#print(number)
+
+pc_choise = number
+user_choise = input('rock, paper, or scissors?')
+
+if user_choise == 'rock':
+    if pc_choise == 0:
+        print('The computer also chose rock. Draw.')
+    elif pc_choise == 1:
+        print ('The computer chose scissors! Rock crushes scissors! You win!')
+    else:
+        print ('The computer choose paper. Paper sufficates rock. Computer won!')
+elif user_choise == 'scissors':
+    if pc_choise == 0:
+        print('The computer chose rock. Rock crushes scissors! Computer won!')
+    elif pc_choise == 1:
+        print('The computer also chose scissors. Draw.')
+    else:
+        print('The computer chose paper. Scissors cut paper! You win!')
+elif user_choise == 'paper':
+    if pc_choise == 0:
+        print('The computer chose rock. Paper sufficates rock. You win!')
+    elif pc_choise == 1:
+        print('The computer chose scissors. Scissors cut paper! Computer won!')
+    else: 
+        print('The computer also chose paper. Draw.')
+else:
+    print('I do not understand.')
