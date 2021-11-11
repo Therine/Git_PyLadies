0d1516786c168adcb6e9cbddcc8648acb05f3f03
Therine <krpdelong@gmail.com>
2021-11-11 10:14:13 -0600

    Added while True: loop and there was nothing to stop the loop. I want it to stop when the sum of the modulo set is 0 and the length of the modulo set equals the factor_count variable

--- 04/smallest_multiple.py
+++ 04/smallest_multiple.py
@@ -1,13 +1,13 @@
 modulo_set = [] # the smallest number that is divisible by each number up to and including range_stop
 factor_count = 10 # the number of factors that should be divisible
 smallest_multiple = 1
-#while sum(modulo_set) != 0: # the first smallest multiple have have all 0 in their modulo set.:  
-for i in range (1,factor_count): # I want to put this inside of another for loop, but I don't want to define the range stop of that one. I want the while loop to control when we stop checking divisors
-    smallest_multiple_candidate = 0
-    modulo = smallest_multiple_candidate % i # smallest multiple iterates by one whole number each loop. 
-    modulo_set.append(modulo) # this is how you collect the 0 to evaluate whether all of the iterations divide evenly.
-    
-    smallest_multiple = smallest_multiple_candidate + 1
+while True: # the first smallest multiple have have all 0 in their modulo set.:  
+    for i in range (1,factor_count): # I want to put this inside of another for loop, but I don't want to define the range stop of that one. I want the while loop to control when we stop checking divisors
+        smallest_multiple_candidate = 1
+        modulo = smallest_multiple_candidate % i # smallest multiple iterates by one whole number each loop. 
+        modulo_set.append(modulo) # this is how you collect the 0 to evaluate whether all of the iterations divide evenly.
+        smallest_multiple = smallest_multiple_candidate + 1
 
 print (sum(modulo_set))
+print (modulo_set)
 print (smallest_multiple)
