f7b66b29ac5e6e5d114c53c2831eed47b07a8b75
Therine <krpdelong@gmail.com>
2021-11-11 09:44:45 -0600

    The version creates the modulo list and the correct sum to evaluate it

---
+++ 04/smallest_multiple.py
@@ -0,0 +1,11 @@
+modulo_set = [] # the smallest number that is divisible by each number up to and including range_stop
+factor_count = 10 # the number of factors that should be divisible
+
+#while sum(modulo_set) != 0: # the first smallest multiple have have all 0 in their modulo set.:  
+for i in range (1,factor_count): # I want to put this inside of another for loop, but I don't want to define the range stop of that one. I want the while loop to control when we stop checking divisors
+    modulo = 2520 % i # smallest multiple iterates by one whole number each loop. 
+    modulo_set.append(modulo) # this is how you collect the 0 to evaluate whether all of the iterations divide evenly.
+    
+      #smallest_multiple = smallest_multiple + 1
+
+print (sum(modulo_set))
