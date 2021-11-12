d01d49a8cfd4ffaaec326ddfe5dc6e49613f4359
Therine <krpdelong@gmail.com>
2021-11-12 12:33:48 -0600

    I moved the modulo set variable into the while loop. Now I just need to figure out how to reset this variable after every for loop.

--- 04/smallest_multiple.py
+++ 04/smallest_multiple.py
@@ -1,12 +1,12 @@
-modulo_set = [] # the smallest number that is divisible by each number up to and including range_stop
-
 smallest_multiple = 1
 active = True
 
 while active != False:
-    factor_count = 18 # the number of factors that should be divisible
+    factor_count = 10 # the number of factors that should be divisible
 #while len(modulo_set) != factor_count and sum(modulo_set) != 0:  
+    modulo_set = [] # the smallest number that is divisible by each number up to and including range_stop
     for i in range (1,factor_count): # I want to put this inside of another for loop, but I don't want to define the range stop of that one. I want the while loop to control when we stop checking divisors
+        
         smallest_multiple_candidate = 1
         modulo = smallest_multiple_candidate % i # smallest multiple iterates by one whole number each loop. 
         modulo_set.append(modulo) # this is how you collect the 0 to evaluate whether all of the iterations divide evenly.
@@ -14,8 +14,7 @@ while active != False:
         print (len(modulo_set))
         if len(modulo_set) == factor_count:
             active = False
-                
-   
+
 
 print ('factor count:', factor_count)
 print ('sum of modulo set: ', sum(modulo_set))
