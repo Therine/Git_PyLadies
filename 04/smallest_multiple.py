a0834b00fad41dc4d3be94dfd9ff2009191b7d7f
Therine <krpdelong@gmail.com>
2021-11-15 14:31:58 -0600

    Added the outer for loop to try to calculate the modulos of every number after 2 until the lenght of the zero set equals the factor_count, but it didn't work, smallest_multiple not updating each time.

--- 04/smallest_multiple.py
+++ 04/smallest_multiple.py
@@ -2,19 +2,19 @@
 #I would just naively go through numbers starting from 1 and test if it is divisible by prime numbers from 2 to 20 without a remainder. It will take quite some time for 1-20 but will finish eventually.
 modulo_set = [] # this is the evaluation set where i divides into each smallest_multiple, and appends the modulo to this set. 
 zero_set = [] # when the sum of the modulo_set equals zero, append it to this set
-
+smallest_multiple = 2
 factor_count = 10 # the number of factors that should be divisible
-smallest_multiple = 2521
-for i in range (1,factor_count):
-  modulo = smallest_multiple % i
-  modulo_set.append(modulo)
-  print (i)
-  print(modulo_set)
-  print (sum(modulo_set)) #each of these zeros represents an iteration from 1-2520 % i until the set equals zero
-  if sum(modulo_set) == 0:
-    zero_set.append(sum(modulo_set))
-    if len(zero_set) == factor_count:
-      break
+for smallest_multiple in range (smallest_multiple + 1):
+  for i in range (1,factor_count):
+    modulo = smallest_multiple % i
+    modulo_set.append(modulo)
+    print (i)
+    print(modulo_set)
+    print (sum(modulo_set)) #each of these zeros represents an iteration from 1-2520 % i until the set equals zero
+    if sum(modulo_set) == 0:
+      zero_set.append(sum(modulo_set))
+      if len(zero_set) == factor_count:
+        break
 
     
 print (zero_set)
