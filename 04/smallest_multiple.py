cc44ae1a23c7a929e9bd2bbf9f31cc530db9185e
Therine <krpdelong@gmail.com>
2021-11-15 14:28:18 -0600

    added some if clauses to end the program.

--- 04/smallest_multiple.py
+++ 04/smallest_multiple.py
@@ -3,19 +3,19 @@
 modulo_set = [] # this is the evaluation set where i divides into each smallest_multiple, and appends the modulo to this set. 
 zero_set = [] # when the sum of the modulo_set equals zero, append it to this set
 
-#factor_count = 10 # the number of factors that should be divisible
-smallest_multiple = 2
-for j in range (smallest_multiple + 1):
-  for i in range (1,10):
-    modulo = j % i
-    modulo_set.append(modulo)
-    print (i)
-    print(modulo_set)
-    print (sum(modulo_set)) #each of these zeros represents an iteration from 1-2520 % i until the set equals zero
-    if sum(modulo_set) == 0:
-      zero_set.append(sum(modulo_set))
+factor_count = 10 # the number of factors that should be divisible
+smallest_multiple = 2521
+for i in range (1,factor_count):
+  modulo = smallest_multiple % i
+  modulo_set.append(modulo)
+  print (i)
+  print(modulo_set)
+  print (sum(modulo_set)) #each of these zeros represents an iteration from 1-2520 % i until the set equals zero
+  if sum(modulo_set) == 0:
+    zero_set.append(sum(modulo_set))
+    if len(zero_set) == factor_count:
+      break
 
-    smallest_multiple = smallest_multiple + 1
     
 print (zero_set)
 print (smallest_multiple)
