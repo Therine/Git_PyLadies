e046d29d3e6ac3a59f8eca3c32f1581a167307c4
Therine <krpdelong@gmail.com>
2021-11-15 11:45:45 -0600

    moved the modulo set into inner for loop and got nothing

--- 04/smallest_multiple.py
+++ 04/smallest_multiple.py
@@ -7,8 +7,9 @@ factor_count = 10 # the number of factors that should be divisible
 
 #while sum(modulo_set) != 0: # the first smallest multiple have have all 0 in their modulo set.:  
 for j in range(1000):
-  modulo_set = [] # the smallest number that is divisible by each number up to and including range_stop
+  
   for i in range (1,factor_count): # I want to put this inside of another for loop, but I don't want to define the range stop of that one. I want the while loop to control when we stop checking divisors
+      modulo_set = [] # the smallest number that is divisible by each number up to and including range_stop
       smallest_multiple = j
       modulo = smallest_multiple % i # smallest multiple iterates by one whole number each loop. 
       modulo_set.append(modulo) # this is how you collect the 0 to evaluate whether all of the iterations divide evenly.
