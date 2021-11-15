1d2da5f7ba213bee0f05ee2c5500a1f5ff94fb4a
Therine <krpdelong@gmail.com>
2021-11-15 14:00:18 -0600

    just variable-ized the smallest multiple, the one that I know of as 2520

--- 04/smallest_multiple.py
+++ 04/smallest_multiple.py
@@ -4,21 +4,15 @@ modulo_set = [] # this is the evaluation set where i divides into each smallest_
 zero_set = [] # when the sum of the modulo_set equals zero, append it to this set
 
 #factor_count = 10 # the number of factors that should be divisible
+smallest_multiple = 2520
+for i in range (1,10):
+  modulo = smallest_multiple % i
+  modulo_set.append(modulo)
+  print (i)
+  print(modulo_set)
+  print (sum(modulo_set)) #each of these zeros represents an iteration from 1-2520 % i until the set equals zero
 
-#for i in range (1,10):
- # modulo = 2520 % i
-  #modulo_set.append(modulo)
-   # print (i)
-    #print(modulo_set)
-  #print (sum(modulo_set)) #each of these zeros represents an iteration from 1-2520 % 1-10 until the set equals zero
-
-for i in range (2,11):
-  factor_count_modulo = 2520 % i
-  modulo_set.append(factor_count_modulo)
-  print (sum(modulo_set))
-  zero_set.append(sum(modulo_set))
-  print (zero_set)
-
+    
 
 
 #print (sum(modulo_set))
