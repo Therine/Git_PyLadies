27230423426c571c5e32b224888edfa8f1c6e3c9
Therine <krpdelong@gmail.com>
2021-11-15 13:43:17 -0600

    trying to hack the 1-smallest multiple of number of factors iteration.

--- 04/smallest_multiple.py
+++ 04/smallest_multiple.py
@@ -1,28 +1,23 @@
 #pyladies advice
 #I would just naively go through numbers starting from 1 and test if it is divisible by prime numbers from 2 to 20 without a remainder. It will take quite some time for 1-20 but will finish eventually.
-modulo_set = [] # the smallest number that is divisible by each number up to and including range_stop
-
+modulo_set = [] # this is the evaluation set where i divides into each smallest_multiple, and appends the modulo to this set. 
+zero_set = [] # when the sum of the modulo_set equals zero, append it to this set
 
 #factor_count = 10 # the number of factors that should be divisible
 
-#while sum(modulo_set) != 0: # the first smallest multiple have have all 0 in their modulo set.:  
-#for j in range(1000):
-  
- # for i in range (1,factor_count): # I want to put this inside of another for loop, but I don't want to define the range stop of that one. I want the while loop to control when we stop checking divisors
-      
-  
-  #    smallest_multiple = j
-  #   modulo = smallest_multiple % i # smallest multiple iterates by one whole number each loop. 
-  #   modulo_set.append(modulo) # this is how you collect the 0 to evaluate whether all of the iterations divide evenly.
-  #   print(modulo_set)
-     
-      #smallest_multiple = smallest_multiple + 1
-for i in range (1,10):
-  modulo = 2520 % i
-  modulo_set.append(modulo)
-    #print (i)
+#for i in range (1,10):
+ # modulo = 2520 % i
+  #modulo_set.append(modulo)
+   # print (i)
     #print(modulo_set)
+  #print (sum(modulo_set)) #each of these zeros represents an iteration from 1-2520 % 1-10 until the set equals zero
+
+for i in range (2,11):
+  factor_count_modulo = 2520 % i
+  modulo_set.append(factor_count_modulo)
   print (sum(modulo_set))
+  zero_set.append(sum(modulo_set))
+  print (zero_set)
 
 
 
