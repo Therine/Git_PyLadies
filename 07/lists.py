f5148f8627cebb41d8879259e9c3cb53174e49ef
Therine <krpdelong@gmail.com>
2021-11-16 11:32:42 -0600

    adding a file for lesson 7

---
+++ 07/lists.py
@@ -0,0 +1,7 @@
+#lists are used to store different items in one variable.
+#lists start with square brackets []
+numbers = [1, 1, 2, 3, 5, 8, 13]
+print(numbers)
+
+for number in numbers:
+    print(number)
