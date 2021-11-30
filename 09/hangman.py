1f39229f6a3b18f1b0c283f3e696687359c4ba2b
Therine <krpdelong@gmail.com>
2021-11-30 12:09:52 -0600

    Initial commit with instructions from the course commented out

---
+++ 09/hangman.py
@@ -0,0 +1,14 @@
+#6. Game time! Let’s create a hangman game together 
+#The game should look like this. Separate the project into multiple functions, take care of good descriptive names and write comments (for future you). You should also put everything to git after every step. 
+#• Computer will randomly choose a word (as a beginning out of 3 options). Also for simplicity use lower case and only words where a letter is used only once. Eg. Mentoring or Pyladies :)
+#• Default setting of game “status” is a string (or a list) with that many underscores as there are letters in the chosen word. 
+#• Default setting of unsuccessful tries is zero. (as a counter of guesses) • Keep repeating: 
+#o Ask the player for a letter. 
+#o If the letter is in the chosen word, replace corresponding 
+#underscores with letters in the game status. Write a function for this. 
+#o If the letter is not present, add one to unsuccessful tries. 
+#o Print out the game “status” – string with underscores and already revealed letters or still just underscores. 
+#o If in the string are no underscores, congratulate a player and end the game. 
+#o Print out the number of unsuccessful tries and (if you want) print out the corresponding picture of a hangman. 
+#o If the number of unsuccessful tries is 9 or higher, the player loses the game and ends the game.
+
