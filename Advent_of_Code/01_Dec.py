e7fd00c7d81efe0212087811fd979ea393463a92
Therine <krpdelong@gmail.com>
2021-12-01 13:59:13 -0600

    calculated the difference between [1] and [0] by converting the variables into int.

--- Advent_of_Code/01_Dec.py
+++ Advent_of_Code/01_Dec.py
@@ -1,7 +1,12 @@
 #How many measurements are larger than the previous measurement?
+#Code from Tyna to use the data in the file that I downloaded from Advent of Code
 with open ("input.txt") as f:
     lines = f.read().splitlines()
 
-for i in lines:
-    difference = lines[(i)] - lines[(i-1)]
-    print(difference)
+#prestep, can I even find the difference between 2 of these items in the lines list?
+difference = int(lines[1]) - int(lines[0])
+print(difference)
+#First, I need to iterate over each measurement in order to calculate the difference
+#for i in lines:
+#    difference = i - lines[:1]
+#    print(difference)
