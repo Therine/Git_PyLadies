fdd95915d0c8f5daa76fb6fc0325cc3969e8cb1d
Therine <krpdelong@gmail.com>
2021-12-01 14:47:43 -0600

    the for loop calculated something, but I don't think that it was the difference between each element. Plus it broke with an error: IndexError: list index out of range

--- Advent_of_Code/01_Dec.py
+++ Advent_of_Code/01_Dec.py
@@ -6,17 +6,17 @@ with open ("input.txt") as f:
     for i in range(0, len(lines)):
         lines[i] = int(lines[i])
 #hopefully it isn't a set of strings?
-print(lines)
-
+#print(lines)
+#Cool, looks like integers now!
 #prestep, can I even find the difference between 2 of these items in the lines list?
 #difference = int(lines[1]) - int(lines[0])
 #print(difference)
 #A really bizarre way to figure this out is to make one more list from these files and just compare each index against each other. 
-#difference = []
+deeper = []
 #First, I need to iterate over each measurement in order to calculate the difference
-#for i in lines:
-    #difference = int(lines[:-1]) - int(lines[1:])
-    #print(difference)
+for i in lines:
+    difference = lines[i] - lines[i-1]
+    print(difference)
 
 #idea found here: https://stackoverflow.com/questions/2400840/python-finding-differences-between-elements-of-a-list
 #result = [j-i for i, j in zip(int(lines[:-1]), int(lines[1:]))]
