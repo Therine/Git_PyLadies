808d00af30f1347da52d979bae6de6eb79a9797e
Therine <krpdelong@gmail.com>
2021-12-01 14:02:59 -0600

    added the static difference calculation into the for loop

--- Advent_of_Code/01_Dec.py
+++ Advent_of_Code/01_Dec.py
@@ -4,9 +4,9 @@ with open ("input.txt") as f:
     lines = f.read().splitlines()
 
 #prestep, can I even find the difference between 2 of these items in the lines list?
-difference = int(lines[1]) - int(lines[0])
-print(difference)
+#difference = int(lines[1]) - int(lines[0])
+#print(difference)
 #First, I need to iterate over each measurement in order to calculate the difference
-#for i in lines:
-#    difference = i - lines[:1]
-#    print(difference)
+for i in lines:
+    difference = int(lines[1]) - int(lines[0])
+    print(difference)
