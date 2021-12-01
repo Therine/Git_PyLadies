d60364b71e536b8cca042b57983b94214bdd3ed3
Therine <krpdelong@gmail.com>
2021-12-01 15:01:58 -0600

    starting to add Lubo's progress marker

--- Advent_of_Code/01_Dec.py
+++ Advent_of_Code/01_Dec.py
@@ -12,13 +12,17 @@ with open ("input.txt") as f:
 #difference = int(lines[1]) - int(lines[0])
 #print(difference)
 #A really bizarre way to figure this out is to make one more list from these files and just compare each index against each other. 
-deeper = []
+deeper = 0
 #First, I need to iterate over each measurement in order to calculate the difference
 for i in range(1, len(lines)):
     difference = lines[i] - lines[i-1]
+    if difference 
     print(difference)
 
 #idea found here: https://stackoverflow.com/questions/2400840/python-finding-differences-between-elements-of-a-list
 #result = [j-i for i, j in zip(int(lines[:-1]), int(lines[1:]))]
 # I can't get it to run!
+
+#okey, now I have to Lubo's progress marker thing
+
 
