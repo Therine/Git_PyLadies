4baa5b5325d2e3d1f54d3de5ac7a0fd351187e3e
Therine <krpdelong@gmail.com>
2021-12-03 18:44:04 -0600

    no progress

--- Advent_of_Code/03_Dec_pt_1.py
+++ Advent_of_Code/03_Dec_pt_1.py
@@ -28,7 +28,7 @@ for j in range(0,12):
         
         gamma_bit = lines[i][j]
         gamma_position.append(gamma_bit)
-       # print(gamma_bit)
+    # print(gamma_bit)
     #The epsilon rate is calculated in a similar way; rather than use the most common bit, 
     #the least common bit from each position is used. 
     #len(lines) = 1000
@@ -44,53 +44,20 @@ for j in range(0,12):
     if sum(gamma_position)<=500:
         gamma_list.append(0)
     gamma_position = []
-
-
-#print(gamma_list)
 gamma_rate = ''.join(map(str,gamma_list))
-print(gamma_rate)
+int(gamma_rate)
 binary_string = gamma_rate
 
 try:
-    gamma_rate_decimal = int(binary_string,2)  
-    print("The decimal value is :", gamma_rate_decimal)    
-    
+    gamma_rate_decimal = int(binary_string,2)    
+
 except ValueError:
     print("Invalid binary number")
 
-for j in range(0,12):
-    for i in range(len(lines)):
-        
-        epsilon_bit = lines[i][j]
-        epsilon_position.append(epsilon_bit)
-       # print(epsilon_bit)
-    #The epsilon rate is calculated in a similar way; rather than use the most common bit, 
-    #the least common bit from each position is used. 
-    #len(lines) = 1000
-    #len(lines[0]) = 12
-    ##change the strings into integers
-    for i in range(0, len(epsilon_position)):
-        epsilon_position[i] = int(epsilon_position[i])
-    #translate the binary of gamma_rate into decimal
-    #print(gamma_position)
-    #print(sum(epsilon_position))
-    if sum(epsilon_position) <= 500:
-        epsilon_list.append(1)
-    if sum(epsilon_position)>500:
-        epsilon_list.append(0)
-    epsilon_position = []
+print(binary_string)
+print(gamma_rate_decimal)
 
 
-#print(gamma_list)
-epsilon_rate = ''.join(map(str,gamma_list))
-print(epsilon_rate)
-binary_string = epsilon_rate
 
-try:
-    epsilon_rate_decimal = int(binary_string,2)  
-    print("The decimal value is :", epsilon_rate_decimal)    
-    
-except ValueError:
-    print("Invalid binary number")
-power_consumption = gamma_rate_decimal * epsilon_rate_decimal
-print('power consumption: ', power_consumption)
+#power_consumption = gamma_rate_decimal * epsilon_rate_decimal
+#print('power consumption: ', power_consumption)
