42080ea26d7739e64f8666d3cb96a39d3b27fb0b
Therine <krpdelong@gmail.com>
2021-12-04 11:28:20 -0600

    Function works to create a subset of the lines list to be reused in the next 11 iterations

--- Advent_of_Code/03_Dec_pt_2.py
+++ Advent_of_Code/03_Dec_pt_2.py
@@ -21,28 +21,33 @@ ogr_list = [] #This is where I'm going to put my first set after the line[0] bit
 #Evaluate which is the most common bit (1 or 0) in lines[0] 
 #Append the original element from lines if the first bit is most common to the ogr_list
 #Use the ogr_list for all subsequent bit evaluations
-#I need a function that takes the 1000 elements and creates the sublist of elements that coorespond to the most common bit
-for m in range(0,12):
+#I need a function that takes the 1000 elements and creates the sublist of elements 
+# that coorespond to the most common bit
+def MCBit(eval_list,idx):
     bit_position = [] #This is how I'm going to examine the bit in each position 0-11. Take the sum
     # of this list and because it is binary, you can calculate the most common by whether the sum is more than 500
-    for i in range(0,len(lines)):
-        bit = lines[i][m]
+    for i in range(0,len(eval_list)):
+        bit = eval_list[i][idx]
         bit_position.append(bit)   
 
     #len(lines) = 1000
     #len(lines[0]) = 12
     for j in range(0,len(bit_position)):
         bit_position[j] = int(bit_position[j])
-    print (sum(bit_position))
     if sum(bit_position) >= 500:
     #it needs to got through and evaluate each element of lines 
     # in order to populate the ogr_list properly
         most_frequent_bit = '1'
-        for k in range (0,len(lines)):
-            if lines[k][m] == most_frequent_bit:
-                ogr_list.append(lines[k])
+        for k in range (0,len(eval_list)):
+            if eval_list[k][idx] == most_frequent_bit:
+                ogr_list.append(eval_list[k])
             
     bit_position = []
+    return ogr_list
+#for m in range(0,12):
+MCBit(lines,0)   
+#for p in range(1,len(ogr_list)):
+#    MCBit(ogr_list,p)
 
 print(len(ogr_list))
 print(ogr_list)
