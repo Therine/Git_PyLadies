18ec154cfe22a331a9b2c96a30c0c392ab67c904
Therine <krpdelong@gmail.com>
2021-12-05 14:38:48 -0600

    I'm forgetting everythinggit add . going to back to what I had last night before I went to bed

--- Advent_of_Code/04_Dec_pt_1.py
+++ Advent_of_Code/04_Dec_pt_1.py
@@ -4,12 +4,16 @@ with open ("input_04-dec.txt") as f:
 #change the strings into integers
 #for i in range(0, len(lines)):
 #    lines[i] = int(lines[i])
-#print(lines)
+print(lines)
+
 #print(lines[0])
 drawn_numbers = lines[0].split(',')
-cards = lines[1:2501]
-#space = rows[0].split(' ')
+cards = lines[1]
 print(cards)
+#space = rows[0].split(' ')
+#make a matrix out of the cards variable with nested lists. Rows will be the inner list and cards with be the next list and all the cards is lines[1:2501]
+#insert()	inserts an item at the defined index
+#print(rows[0])
 #something that can store the space of the card and whether it have been drawn
 #print(boards)
 #print(drawn_numbers)
