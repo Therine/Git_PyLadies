98e89d46d948356fcab20ec66c1d873df2c5f2cc
Therine <krpdelong@gmail.com>
2021-12-05 14:15:40 -0600

    trying to make the lines[1:2501] into a set of 100 matrices 5x5

--- Advent_of_Code/04_Dec_pt_1.py
+++ Advent_of_Code/04_Dec_pt_1.py
@@ -7,6 +7,18 @@ with open ("input_04-dec.txt") as f:
 #print(lines)
 #print(lines[0])
 drawn_numbers = lines[0].split(',')
-boards = lines[1:2501]
+cards = lines[1:2501]
+#space = rows[0].split(' ')
+print(cards)
+#something that can store the space of the card and whether it have been drawn
 #print(boards)
-print(drawn_numbers)
+#print(drawn_numbers)
+# we need to iterate through the drawn_numbers list to mark each number that comes up. 
+# I need to define what it means to get Bingo
+# Like, if indexes in groups of 5 are marked () = bingo
+# if indexes in multiples of 5 are marked 
+#for i in boards:
+#    if i == drawn_numbers[int(i)]:
+#        i.append('x')
+
+#print(boards)
