e373a4c0450b0af97321bc3ccf9b9a347b95f427
Therine <krpdelong@gmail.com>
2022-01-25 13:20:55 -0600

    this created a list of periods without any line breaks, I think that I need to create the bracketed matrix so I have a fighting chance of getting identifying each period.

--- Snake/snake_game.py
+++ Snake/snake_game.py
@@ -1,19 +1,16 @@
+#https://pyladiesvienna.pythonanywhere.com/
+
 #Creates a 10x10 table with nest tuples [['.','.','.'],['.','.','.'],['.','.','.']]
 #So essentially a matrix
-#def draw_map():
-#    print([['.','.','.'],['.','.','.'],['.','.','.']])
+def draw_map():
+   print([['.','.','.'],['.','.','.'],['.','.','.']])
 
-#draw_map()
-R = int(input("Enter the number of rows:")) 
-C = int(input("Enter the number of columns:")) 
-  
-# Initialize matrix 
-matrix = [] 
-print("Enter the entries rowwise:") 
-  
-# For user input 
-for i in range(R):          # A for loop for row entries 
-    a =[] 
-    for j in range(C):      # A for loop for column entries 
-         a.append((input())) 
-    matrix.append(a) 
+draw_map()
+
+for x in range(10):
+    for y in range(10):
+        print('.', end=" ")
+        if (x,y) in [(0,0),]:
+            print('x', end=" ")
+        else:
+            print('.', end=" ")
