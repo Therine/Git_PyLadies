785621c0b09586478c0a880ed43b5234f27192f5
Katherine DeLong <krpdelong@gmail.com>
2022-04-17 22:31:37 -0500

    made a while loop that let me play each turn, but completely excluded the computer

--- Tic-tac-toe_no_functions.py
+++ Tic-tac-toe_no_functions.py
@@ -14,12 +14,23 @@
 # 14. If the position is a mark, display that the position is already filled and the player needs to pick a new position number. 
 # 15. After each mark is added, check to see if the board has any xxx or ooo in the string.
 # 16. Once there is an xxx or an ooo, announce that the player with that mark won and ask if they want to play again. Restart at the beginning if yes and exit game if not.
+from random import randrange
 board = "--------------------"
+play_again = True
 print("Let's play a nice game of tic-tac-toe,\nerr maybe it is really just tic?\nThere is only one dimension of 20 positions, and it looks like this\n", board )
 player_mark_input = input("Anyway, would you like to be an x or o? ")
 # Trying to learn how use the variables pc_mark and player_mark outside of this assign_mark function.
 # https://www.kite.com/python/answers/how-to-access-a-variable-outside-of-a-function-in-python
 # 
+def evaluate(board):
+    if board == "%xxx%":
+        return "X"
+    elif board == "%ooo%":
+        return "O"
+    elif board != "%-%":
+        return "!"
+    else:
+        return "-"
 
 def assign_mark(player_mark_input):  
   if player_mark_input == "X" or "x":
@@ -31,16 +42,30 @@ def assign_mark(player_mark_input):
   return (pc_mark, player_mark)
 print("Great, you'll be", assign_mark(player_mark_input)[1], "and I'll be", assign_mark(player_mark_input)[0])
 goes_first = input("So, which one of you should go first, you or the computer. Type in 'me' or 'pc': ")
-if goes_first == 'me':
-    position = int(input("Pick a number between 0 and 19: "))
-    if board[position] == "-":  
-      temp = list(board)
-      temp[position] = assign_mark(player_mark_input)[1]
-      board = "".join(temp)
-    else:
-      print("There is already a mark at that spot.")
-      
-print(board)
+while play_again:
+  if goes_first == 'me':
+        position = int(input("Pick a number between 0 and 19: "))
+        if board[position] == "-":  
+          temp = list(board)
+          temp[position] = assign_mark(player_mark_input)[1]
+          board = "".join(temp)
+          evaluate(board)
+        else:
+          print("There is already a mark at that spot.")
+  else:
+        position = randrange(0,19,1)
+        if board[position] == "-":  
+          temp = list(board)
+          temp[position] = assign_mark(player_mark_input)[1]
+          board = "".join(temp)
+        else:
+          print("There is already a mark at that spot.")
+  print(board)
+  play_again = input("Do you want to keep playing? Type True or False: ")
+
+
+
+
 
 
 
