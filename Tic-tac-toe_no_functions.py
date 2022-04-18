bbd0d50e68da8f71058884d317693b74d86d2862
Katherine DeLong <krpdelong@gmail.com>
2022-04-17 19:57:28 -0500

    started a new file without functions to see if I can at least get it all the way through

---
+++ Tic-tac-toe_no_functions.py
@@ -0,0 +1,17 @@
+# 1. Introduce the game. 
+# 2. Find out which mark the human wants to use.
+# 3. Assign the other mark to the computer.
+# 4. Display the tic-tac-toe board empty with 20 dashes.
+# 5. Find out who should go first.
+# 6. If it is the human, ask them where they want to place their marker. 
+# 7. Update the board with the mark in the that place.
+# 8. If it is the computer, run the random number generater between 0-19. 
+# 9. Display the board with the new marker.
+# 10. Next move and all future moves. 
+# 11. Alternate players
+# 12. Ask the player (run the random generator) for a position number.
+# 13. If the position is a dash, place the mark.
+# 14. If the position is a mark, display that the position is already filled and the player needs to pick a new position number. 
+# 15. After each mark is added, check to see if the board has any xxx or ooo in the string.
+# 16. Once there is an xxx or an ooo, announce that the player with that mark won and ask if they want to play again. Restart at the beginning if yes and exit game if not.
+
