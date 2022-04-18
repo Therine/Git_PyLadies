b1ac1180cb9f1c6e6a6f11ec86441b5ff8e79154
Katherine DeLong <krpdelong@gmail.com>
2022-04-17 20:11:37 -0500

    Rewrote the steps that I want to take, there is already a function but it is because it is a great function.

--- Tic-tac-toe_no_functions.py
+++ Tic-tac-toe_no_functions.py
@@ -14,4 +14,23 @@
 # 14. If the position is a mark, display that the position is already filled and the player needs to pick a new position number. 
 # 15. After each mark is added, check to see if the board has any xxx or ooo in the string.
 # 16. Once there is an xxx or an ooo, announce that the player with that mark won and ask if they want to play again. Restart at the beginning if yes and exit game if not.
+board = "--------------------"
+print("Let's play a nice game of tic-tac-toe,\nerr maybe it is really just tic?\nThere is only one dimension of 20 positions, and it looks like this\n", board )
+player_mark_input = input("Anyway, would you like to be an x or o? ")
+# Trying to learn how use the variables pc_mark and player_mark outside of this assign_mark function.
+# https://www.kite.com/python/answers/how-to-access-a-variable-outside-of-a-function-in-python
+# 
 
+def assign_mark(player_mark_input):  
+  if player_mark_input == "X" or "x":
+    pc_mark = "o"
+    player_mark = "x"
+  elif player_mark_input == "0" or "o" or "O":
+    pc_mark = "x"
+    player_mark = "o"
+  return (pc_mark, player_mark)
+print("Great, you'll be", assign_mark(player_mark_input)[1], "and I'll be", assign_mark(player_mark_input)[0])
+goes_first = input("So, which one of you should go first, you or the computer. Type in 'me' or 'pc': ")
+if goes_first == 'me':
+    position = input("Pick a number between 0 and 19: ")
+    
