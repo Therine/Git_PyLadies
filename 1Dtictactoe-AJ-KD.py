5cf61a473e4d08ab36689f0752ce1dacf7ba8651
Katherine DeLong <krpdelong@gmail.com>
2022-04-17 19:27:41 -0500

    getting reacquainted with this program again

--- 1Dtictactoe-AJ-KD.py
+++ 1Dtictactoe-AJ-KD.py
@@ -19,7 +19,6 @@ board = '--------------------'
 #Step 1
 #Write a function evaluate that gets the string with the board of 1D tic-tac-toe, and returns one character based on the state of the game:
 def evaluate(board):
-    print(board)
     if board == "%xxx%":
         return "X"
     elif board == "%ooo%":
@@ -33,20 +32,20 @@ def evaluate(board):
 #"!" – Draw (the board is full but nobody has won)
 #"-" – Rest (i.e. the game is not finished)
 #Step 2
-#Write a move function that gets the string with the game board, a position number (0-19), and a (x or o) mark, and returns a game board (i.e., a string with the given mark placed in the given position). The function header should look something like this:
+#Write a move function that gets the string with the game board, a position number (0-19), and a (x or o) mark, and returns a game board 
+# (i.e., a string with the given mark placed in the given position). The function header should look something like this:
 
 def move(board, mark, position):
     # Returns the game board with the given mark in the given position.
-    print ("Let's take a look at the board, shall we?")
-    evaluate(board)
-    if 
-    print("Right, that line of dashes is our board. As you play, the board should update \n with your marker and then the computer's marker until one of you get 3 in a row.")
-    
+          
     if goes_first == "me":
         player_move(position = int(input("Where you would like to place your piece (0-19)? ")))
+        for i in board:
+          if board[0] = 
 
 #Step 3
-#Write a player_move function that gets a string with the game board, asks the player which position he wants to play, and returns the updated game board with the player's move. The function should reject negative or too large numbers or moves to an occupied position. If the user has entered a wrong argument, the function should ask again (to get correct answer).
+#Write a player_move function that gets a string with the game board, asks the player which position he wants to play, 
+# and returns the updated game board with the player's move. The function should reject negative or too large numbers or moves to an occupied position. If the user has entered a wrong argument, the function should ask again (to get correct answer).
 #this function needs work. 
 def player_move(board, position):
   chosen_position = board[position]
